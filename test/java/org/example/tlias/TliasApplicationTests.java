e285450f046c44967db0bc4cc0bacf6f833e7d09
Hui Wang <148246813+shuisuhi@users.noreply.github.com>
2025-08-11 09:10:44 +0800

    Add files via upload

---
+++ test/java/org/example/tlias/TliasApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.tlias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.io.ByteArrayInputStream;
+
+@SpringBootTest
+class TliasApplicationTests {
+    @Test
+    void testString(){
+
+    }
+
+}
